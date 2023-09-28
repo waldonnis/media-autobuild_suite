8959ecea34b86516c0dc2401b074a651d69f7375
waldonnis <ghostchris@gmail.com>
2023-09-28 14:19:59 -0400

    Re-allow suite to apply the dedupe ffmpeg patch I overrode it before because I was patching configure as well which caused issues because git am doesn't do fuzzy matching.  I'm no longer patching ffmpeg locally at all, so no need to use a local version of the dedupe patch.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2162,10 +2162,10 @@ if [[ $ffmpeg != no ]]; then
             unset _openal_flag
         fi
 
-        #if [[ ${#FFMPEG_OPTS[@]} -gt 35 ]]; then
+        if [[ ${#FFMPEG_OPTS[@]} -gt 35 ]]; then
             # remove redundant -L and -l flags from extralibs
-            #do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/ffmpeg/0001-configure-deduplicate-linking-flags.patch" am
-        #fi
+            do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/ffmpeg/0001-configure-deduplicate-linking-flags.patch" am
+        fi
 
         _patches=$(git rev-list origin/master.. --count)
         [[ $_patches -gt 0 ]] &&
