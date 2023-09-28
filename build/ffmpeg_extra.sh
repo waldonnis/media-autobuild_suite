33ece25c1900a11400d14ecabb205287bf36f2ba
waldonnis <ghostchris@gmail.com>
2023-09-28 14:22:36 -0400

    Remove deprecated patches ...and the ffmpeg_extra script that used them.

--- build/ffmpeg_extra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-_post_vcs(){
-    # Hard-code cuda arch to sm_86/compute_86
-    patch -p1 -i "$LOCALBUILDDIR/local_patches/ffmpeg_cuda-arch-ampere.patch"
-    patch -p1 -i "$LOCALBUILDDIR/local_patches/ffmpeg_configure-deduplicate-linking-flags-rebase.patch"
-}
-
