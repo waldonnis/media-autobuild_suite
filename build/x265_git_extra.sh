700c1bb1de85eba117db6d76c1d87e6cb02caee4
waldonnis <ghostchris@gmail.com>
2023-11-08 00:18:56 -0500

    Fold x265 build changes into fork Removed native compilation patch since I set tune/arch targets globally

--- build/x265_git_extra.sh
+++ build/x265_git_extra.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 _post_vcs(){
-    # Change default to a native build (enable NATIVE_BUILD), enable LTO by default
-    # and apply a rebased library paths patch since the suite's version fails after my patches
-    patch -p1 -i "$LOCALBUILDDIR/local_patches/x265_native-compilation.patch"
+    # Enable LTO by default and apply a rebased library paths patch since the suite's
+    # version fails after my patches
     patch -p1 -i "$LOCALBUILDDIR/local_patches/x265_lto_enable.patch"
     patch -p1 -i "$LOCALBUILDDIR/local_patches/x265_rebased-library-paths-patch.patch"
 }
