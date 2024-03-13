a3915570e1d4e20049e7e4d77b39f3f608b9fc4c
waldonnis <ghostchris@gmail.com>
2024-03-13 00:33:44 -0400

    mpv_extra.sh: Disable temp patch - didn't work

--- build/mpv_extra.sh
+++ build/mpv_extra.sh
@@ -2,6 +2,6 @@
 
 _post_vcs(){
     # Temp fix for libav changes
-    patch -p1 -i "$LOCALBUILDDIR/local-patches/mpv_channellayout-libav-fix.patch"
+    #patch -p1 -i "$LOCALBUILDDIR/local_patches/mpv_channellayout-libav-fix.patch"
 }
 
