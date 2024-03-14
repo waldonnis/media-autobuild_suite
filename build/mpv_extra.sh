e93661a5e4c61c18bd9b7a6e04714144e122f377
waldonnis <ghostchris@gmail.com>
2024-03-13 20:00:06 -0400

    Hopefully temporary fix for our old mpv pinned release's incompatibility with newer libav/ffmpeg channel layout changes local_patches/mpv_channellayout-libav-fix.patch: Deleted

--- build/mpv_extra.sh
+++ build/mpv_extra.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 _post_vcs(){
-    # Temp fix for libav changes
-    #patch -p1 -i "$LOCALBUILDDIR/local_patches/mpv_channellayout-libav-fix.patch"
+    # [PATCH] filters/f_lavfi: handle removed AV_OPT_TYPE_CHANNEL_LAYOUT
+    do_patch "https://github.com/mpv-player/mpv/commit/78447c4b91634aa91dcace1cc6a9805fb93b9252.patch"
+    # [PATCH] filters/f_lavfi: rename channellayout to ch_layout
+    do_patch "https://github.com/mpv-player/mpv/commit/414ddbd628724df3afc1e15f5e415dbb2c76a0b5.patch"
+    # [PATCH] swresample: stop using deprecated {in,out}_channel_layout options
+    do_patch "https://github.com/mpv-player/mpv/commit/9ef614d6a3b5e56474ec91626b06247ac60ed746.patch"
 }
 
