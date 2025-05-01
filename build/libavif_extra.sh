92bebaf632ec63dafaad67d08853667c36b802a5
waldonnis <ghostchris@gmail.com>
2025-05-01 17:51:59 -0400

    Clean-up: Remove outdated libavif patch

--- build/libavif_extra.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-_post_vcs(){
-    # Temporarily remove api version checks for 3.0-specific features since 3.0 hasn't been released yet
-    patch -p1 -i "$LOCALBUILDDIR/local_patches/libavif_api_version_ignore_TEMP.patch"
-}
-
