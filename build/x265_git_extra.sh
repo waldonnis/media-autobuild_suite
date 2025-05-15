70b12a6d89d5265f5d708edf439123df6ad1f3e3
waldonnis <ghostchris@gmail.com>
2025-05-15 14:53:31 -0400

    Remove long-unneeded library path patch for x265

--- build/x265_git_extra.sh
+++ build/x265_git_extra.sh
@@ -4,6 +4,5 @@ _post_vcs(){
     # Enable LTO by default and apply a rebased library paths patch since the suite's
     # version fails after my patches
     patch -p1 -i "$LOCALBUILDDIR/local_patches/x265_lto_enable.patch"
-    patch -p1 -i "$LOCALBUILDDIR/local_patches/x265_rebased-library-paths-patch.patch"
 }
 
