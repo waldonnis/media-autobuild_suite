3dfab5bb571aade3bf28050a3ac846fe86816731
waldonnis <ghostchris@gmail.com>
2026-09-08 12:10:30 -0400

    Force single-threaded building of ffmpeg for now

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2688,7 +2688,7 @@ if [[ $ffmpeg != no ]]; then
                 --bindir="$LOCALDESTDIR/bin-video" "${FFMPEG_OPTS[@]}"
             # cosmetics
             sed -ri "s/ ?--($sedflags)=(\S+[^\" ]|'[^']+')//g" config.h
-            do_make && do_makeinstall
+            do_make -j1 && do_makeinstall -j1
             ! disabled_any debug "debug=gdb" &&
                 create_debug_link "$LOCALDESTDIR"/bin-video/ff{mpeg,probe,play}.exe
             cd_safe ..
